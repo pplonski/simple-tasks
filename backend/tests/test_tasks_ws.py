9b716f9884be02a8fd642b1523cc5629d7e67bb3
Piotrek <pplonski86@gmail.com>
2018-10-24 10:54:21 +0200

    update

--- backend/tests/test_tasks_ws.py
+++ backend/tests/test_tasks_ws.py
@@ -8,7 +8,7 @@ from test_tasks_base import TestTasksBase
 
 class TestTasksWebSocket(TestTasksBase):
 
-    def test_progress(self):
+    def test_ws_progress(self):
 
         ws = create_connection(self.get_server_ws())
 
@@ -22,13 +22,22 @@ class TestTasksWebSocket(TestTasksBase):
         self.assertTrue('db_id' in result['data'])
         self.assertEqual(task['id'], result['data']['db_id'])
 
-    def test_fail_with_exception(self):
+    def test_ws_ends_success(self):
+        pass
+
+
+    def test_ws_fail_with_exception(self):
 
         ws = create_connection(self.get_server_ws())
 
         task = self.create_task(2,-2)
         self.assertEqual(task['state'], 'CREATED')
+
         result =  json.loads(ws.recv())
+
+        if 'PROGRESS' == result['data']['state']:
+            result =  json.loads(ws.recv())
+
         ws.close()
 
         self.assertTrue('data' in result)
@@ -36,13 +45,19 @@ class TestTasksWebSocket(TestTasksBase):
         self.assertTrue('db_id' in result['data'])
         self.assertEqual('FAILURE', result['data']['state'])
 
-    def test_fail_with_segfault(self):
+    def test_ws_fail_with_segfault(self):
 
         ws = create_connection(self.get_server_ws())
 
         task = self.create_task(-2,2)
         self.assertEqual(task['state'], 'CREATED')
         result =  json.loads(ws.recv())
+
+
+        if 'PROGRESS' == result['data']['state']:
+            result =  json.loads(ws.recv())
+
+
         ws.close()
 
         self.assertTrue('data' in result)
