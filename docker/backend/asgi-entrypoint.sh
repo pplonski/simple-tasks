d8b5f518f22d4e5c430991ca6af4d7c9de7bd599
Piotrek <pplonski86@gmail.com>
2018-10-25 11:03:41 +0200

    set nginx config for asgi,wsgi

--- docker/backend/asgi-entrypoint.sh
+++ docker/backend/asgi-entrypoint.sh
@@ -13,6 +13,4 @@ do
 done
 
 
-
-#./manage.py runserver 0.0.0.0:9000
 daphne server.asgi:application --bind 0.0.0.0 --port 9000
--- docker/backend/wsgi-entrypoint.sh
+++ docker/backend/wsgi-entrypoint.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
-cd /app/backend/server
-./manage.py migrate
+until cd /app/backend/server
+do
+    echo "Waiting for server volume..."
+done
+
+until ./manage.py migrate
+do
+    echo "Waiting for postgres ready..."
+    sleep 2
+done
 
 gunicorn server.wsgi --bind 0.0.0.0:8000 --workers 4 --threads 4
-
-#./manage.py runserver 0.0.0.0:8000 # --settings=djangoreactredux.settings.dev_docker
+#./manage.py runserver 0.0.0.0:8000 # --settings=settings.dev_docker
