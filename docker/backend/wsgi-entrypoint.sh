2e5731ea3c1c8e96aaaa51d32d42aaa856fcb2ac
Piotr Plonski <pplonski86@gmail.com>
2018-10-25 21:44:41 +0200

    serving static files

--- docker/backend/wsgi-entrypoint.sh
+++ docker/backend/wsgi-entrypoint.sh
@@ -11,5 +11,7 @@ do
     sleep 2
 done
 
+./manage.py collectstatic --noinput
+
 gunicorn server.wsgi --bind 0.0.0.0:8000 --workers 4 --threads 4
 #./manage.py runserver 0.0.0.0:8000 # --settings=settings.dev_docker
