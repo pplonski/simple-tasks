c0298bf4befe4cbc30b3a70a94dfa236fe1642ac
Piotrek <pplonski86@gmail.com>
2018-10-25 10:48:50 +0200

    acks late in celery

---
+++ backend/worker/celeryconfig.py
@@ -0,0 +1,7 @@
+
+from config import BROKER_URL, REDIS_URL, REDIS_PORT
+
+broker_url = BROKER_URL
+result_backend = 'redis://{0}:{1}'.format(REDIS_URL, REDIS_PORT)
+task_serializer = 'json'
+task_acks_late = True
