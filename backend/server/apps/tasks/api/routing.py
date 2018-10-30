6f585d6fa4a748f2c1f0ce0deb7e12775f6f5995
Łukasz Starnawski <lukasz.starnawski@gmail.com>
2018-10-30 21:41:08 +0100

    change server websocket url

--- backend/server/apps/tasks/api/routing.py
+++ backend/server/apps/tasks/api/routing.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import consumers
 
 websocket_urlpatterns = [
-    url(r'^tasks/$', consumers.TasksConsumer),
+    url(r'^websockets/tasks/$', consumers.TasksConsumer),
 ]
