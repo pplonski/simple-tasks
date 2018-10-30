f584c50f9fd1711f54fab67bfdb8200384fd40e6
Piotrek <pplonski86@gmail.com>
2018-10-30 15:21:14 +0100

    [ci skip]

--- docker/backend/wsgi-entrypoint.sh
+++ docker/backend/wsgi-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+ls -al /app/backend/server/static/client/static/js
+
+
 until cd /app/backend/server
 do
     echo "Waiting for server volume..."
@@ -10,6 +13,10 @@ do
     echo "Waiting for postgres ready..."
     sleep 2
 done
+
+echo '0000000000000000000'
+echo '0000000000000000000'
+echo '0000000000000000000'
 
 
 gunicorn server.wsgi --bind 0.0.0.0:8000 --workers 4 --threads 4
