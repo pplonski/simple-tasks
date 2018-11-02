656f936197036e8e9fa14be4b7d9f911e1729ba4
Piotr Plonski <pplonski86@gmail.com>
2018-11-02 14:11:27 +0100

    setup tests

---
+++ client/src/__tests__/components/tasks/Tasks.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Tasks from '../../../components/tasks/Tasks';
+
+
+function setup() {
+  const props = {
+    getTasks: jest.fn()
+  }
+
+  const enzymeWrapper = shallow(<Tasks {...props} />)
+  return {
+    props,
+    enzymeWrapper
+  }
+}
+
+
+describe('Tasks', () => {
+  describe('Render', () => {
+    it('should render self and subcomponents', () => {
+       console.log('a');
+       const { props, enzymeWrapper } = setup()
+    })
+  })
+})
